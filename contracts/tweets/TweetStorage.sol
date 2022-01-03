3ded5e310be037c9f445dede9cea5a848e61f76f
Cassady <cassady.campos@uleth.ca>
2022-01-02 23:19:09 -0700

    tweet storage

---
+++ contracts/tweets/TweetStorage.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.11;
+
+contract TweetStorage {
+    mapping(uint => Tweet) public tweets;
+
+    struct Tweet {
+        uint id; 
+        string text;
+        uint userId;
+        uint postedAt;
+    }
+
+    uint latestTweetId = 0;
+
+    function createTweet(uint _userId, string memory _text) public returns (uint) {
+        tweets[++latestTweetId] = Tweet(latestTweetId, _text, _userId, block.timestamp);
+
+        return latestTweetId;
+    }
+}
