7bb5c6033f9a9147ca09717d1426837140be6dcb
Cassady <cassady.campos@uleth.ca>
2022-01-02 23:58:33 -0700

    tweet javscript unit test

--- contracts/tweets/TweetStorage.sol
+++ contracts/tweets/TweetStorage.sol
@@ -10,11 +10,10 @@ contract TweetStorage {
         uint postedAt;
     }
 
-    uint latestTweetId = 1;
+    uint latestTweetId = 0;
 
     function createTweet(uint _userId, string memory _text) public returns (uint) {
-        latestTweetId++;
-        require(latestTweetId > 0, 'must be greater than 0 id');
+        require(++latestTweetId > 0, 'must be greater than 0 id');
         tweets[latestTweetId] = Tweet(latestTweetId, _text, _userId, block.timestamp);
 
         return latestTweetId;
